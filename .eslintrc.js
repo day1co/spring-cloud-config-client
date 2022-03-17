118c2c2aa97680b2e918d6277d3dcaf8ec9486a3
moonimooni <heewonmoon@day1company.co.kr>
2022-03-17 16:13:25 +0900

    fix: lint eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,12 @@
 module.exports = {
   env: { jest: true },
-  extends: [
-    "plugin:prettier/recommended",
-    "plugin:@typescript-eslint/recommended",
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: { ecmaVersion: "2021", sourceType: "module" },
-  plugins: ["import", "@typescript-eslint"],
+  extends: ['plugin:prettier/recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: '2021', sourceType: 'module' },
+  plugins: ['import', '@typescript-eslint'],
   root: true,
   rules: {
-    "@typescript-eslint/no-explicit-any": "off",
-    "@typescript-eslint/no-namespace": "off",
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-namespace': 'off',
   },
 };
