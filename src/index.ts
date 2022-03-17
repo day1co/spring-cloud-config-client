1ab8d3c7cb6bfa0029c4296304aad719c721c776
moonimooni <heewonmoon@day1company.co.kr>
2022-03-17 16:16:45 +0900

    to export from src directory

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export { getConfigSync, Config } from './config-client/config-client';
+export { createObjectByFlattenedKey, getNestedObjectValue } from './utils';
+export type {
+  ClientRequestOptions,
+  CloudConfigResponse,
+  ConfigObject,
+  PropertySource,
+} from './config-client/config-client.interface';
