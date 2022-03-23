b026ab272ebd1ecab2912b504dcc28a604a205aa
moonimooni <heewonmoon@day1company.co.kr>
2022-03-23 14:06:23 +0900

    feat: Get multiple applications config at a time from Spring Cloud Config, and merge them as an object, separated by applications' names as a key.
    refactor: Use try/catch in getConfig/getConfigSync function

--- src/config-client/config-client.spec.fixture.ts
+++ src/config-client/config-client.spec.fixture.ts
@@ -1,37 +1,164 @@
-const fixture = {
+const applicationDevelopmentConfig = {
+  name: 'application-development.yml',
+  source: {
+    'database.host': 'application_dev_host',
+    'database.port': 3306,
+    'database.database': 'application_dev',
+  },
+};
+
+const applicationConfig = {
+  name: 'application.yml',
+  source: {
+    'database.host': 'localhost',
+    'database.port': 3306,
+    'database.database': 'application_local',
+    'database.pool.min': 0,
+    'database.pool.max': 10,
+    'database.timezone': 'Z', // this key only exists in application.yml
+  },
+};
+
+const fooDevelopmentConfig = {
+  name: 'foo-development.yml',
+  source: {
+    'database.host': 'foo_dev_host',
+    'database.port': 3306,
+    'database.database': 'foo_dev',
+  },
+};
+
+const fooConfig = {
+  name: 'foo.yml',
+  source: {
+    'database.host': 'localhost',
+    'database.port': 3306,
+    'database.database': 'foo_local',
+    'database.pool.min': 0,
+    'database.pool.max': 10,
+  },
+};
+
+const barDevelopmentConfig = {
+  name: 'bar-development.yml',
+  source: {
+    'database.host': 'bar_dev_host',
+    'database.port': 3306,
+    'database.database': 'bar_dev',
+  },
+};
+
+const barConfig = {
+  name: 'bar.yml',
+  source: {
+    'database.host': 'localhost',
+    'database.port': 3306,
+    'database.database': 'bar_local',
+    'database.pool.min': 0,
+    'database.pool.max': 10,
+  },
+};
+
+/**
+ * @description A mock response from Spring-Cloud-Config-Server. This default config named "application" will always be included in server responses, no matter what your custom config name is.
+ * @see https://docs.spring.io/spring-cloud-config/docs/current/reference/html/#_quick_start
+ */
+export const applicationDevelopmentRawResponse = {
+  name: 'application',
+  profiles: ['development'],
+  propertySources: [applicationConfig, applicationDevelopmentConfig],
+};
+
+/**
+ * @description an expected config which is a merged object with propertySources above.
+ */
+export const applicationDevelopmentMergedConfig = {
+  database: {
+    host: applicationDevelopmentConfig.source['database.host'],
+    port: applicationDevelopmentConfig.source['database.port'],
+    database: applicationDevelopmentConfig.source['database.database'],
+    timezone: applicationConfig.source['database.timezone'],
+    pool: {
+      min: applicationConfig.source['database.pool.min'],
+      max: applicationConfig.source['database.pool.max'],
+    },
+  },
+};
+
+/**
+ * @description a mock response from Spring-Cloud-Config-Server.
+ * @see https://docs.spring.io/spring-cloud-config/docs/current/reference/html/#_quick_start
+ */
+export const fooDevelopmentRawResponse = {
   name: 'foo',
   profiles: ['development'],
+  propertySources: [fooDevelopmentConfig, applicationDevelopmentConfig, fooConfig, applicationConfig],
+};
+
+/**
+ * @description an expected config which is a merged object with propertySources above.
+ */
+export const fooDevelopmentMergedConfig = {
+  database: {
+    host: fooDevelopmentConfig.source['database.host'],
+    port: fooDevelopmentConfig.source['database.port'],
+    database: fooDevelopmentConfig.source['database.database'],
+    timezone: applicationConfig.source['database.timezone'],
+    pool: {
+      min: fooConfig.source['database.pool.min'],
+      max: fooConfig.source['database.pool.max'],
+    },
+  },
+};
+
+/**
+ * @description a mock response from Spring-Cloud-Config-Server.
+ * @see https://docs.spring.io/spring-cloud-config/docs/current/reference/html/#_quick_start
+ */
+export const barDevelopmentRawResponse = {
+  name: 'bar',
+  profiles: ['development'],
+  propertySources: [barDevelopmentConfig, applicationDevelopmentConfig, barConfig, applicationConfig],
+};
+
+/**
+ * @description an expected config which is a merged object with propertySources above.
+ */
+export const barDevelopmentMergedConfig = {
+  database: {
+    host: barDevelopmentConfig.source['database.host'],
+    port: barDevelopmentConfig.source['database.port'],
+    database: barDevelopmentConfig.source['database.database'],
+    timezone: applicationConfig.source['database.timezone'],
+    pool: {
+      min: barConfig.source['database.pool.min'],
+      max: barConfig.source['database.pool.max'],
+    },
+  },
+};
+
+/**
+ * @description An expected response when you use multiple applications, with "name" including ",".
+ * Note that the order of multiple-application response`s propertySources is different from single-appliatoin response`s.
+ */
+export const fooBarDevelopmentRawResponse = {
+  name: 'foo,bar',
+  profiles: ['development'],
   propertySources: [
-    {
-      name: 'foo-development.yml',
-      source: {
-        'database.host': 'foo_dev_host',
-        'database.port': 3306,
-        'database.database': 'foo_dev',
-      },
-    },
-    {
-      name: 'foo.yml',
-      source: {
-        'database.host': 'localhost',
-        'database.port': 3306,
-        'database.database': 'foo_local',
-        'database.pool.min': 0,
-        'database.pool.max': 10,
-      },
-    },
+    fooDevelopmentConfig,
+    fooConfig,
+    barDevelopmentConfig,
+    barConfig,
+    applicationDevelopmentConfig,
+    applicationConfig,
   ],
 };
 
-export const mockData = JSON.stringify(fixture);
-export const mockDataSource = {
-  database: {
-    host: 'foo_dev_host',
-    port: 3306,
-    database: 'foo_dev',
-    pool: {
-      min: 0,
-      max: 10,
-    },
-  },
+/**
+ * @description an expected config which is a merged object with propertySources above.
+ */
+export const fooBarDevelopmentMergedConfig = {
+  application: applicationDevelopmentMergedConfig,
+  foo: fooDevelopmentMergedConfig,
+  bar: barDevelopmentMergedConfig,
 };
--- src/config-client/config-client.ts
+++ src/config-client/config-client.ts
@@ -1,6 +1,5 @@
 import { httpRequestSync, httpRequestAsync, HttpResponse } from '@day1co/http-request-sync';
 import { LoggerFactory, ObjectUtil } from '@day1co/pebbles';
-import type { ObjectType } from '@day1co/pebbles';
 import { createObjectByFlattenedKey, getValueFromNestedObject } from '../utils';
 import {
   DEF_CONFIG_ENDPOINT,
@@ -15,19 +14,9 @@ import type {
   PropertySource,
 } from './config-client.interface';
 
-const logger = LoggerFactory.getLogger('spring-cloud-config-client');
+const DEFAULT_APPLICATION_NAME = 'application';
 
-function createConfigWithHttpResponse(configServerResponse: HttpResponse): Config {
-  try {
-    if (configServerResponse.error) {
-      throw new Error(JSON.stringify(configServerResponse.error));
-    }
-    return Config.getInstance(JSON.parse(configServerResponse.data));
-  } catch (err) {
-    logger.error('Response from config server is not available : %o', configServerResponse);
-    process.exit(1);
-  }
-}
+const logger = LoggerFactory.getLogger('spring-cloud-config-client');
 
 // 환경 변수로 재정의 가능
 // https://docs.spring.io/spring-cloud-config/docs/current/reference/html/#config-data-import
@@ -41,16 +30,33 @@ export function getConfigUrl({
   return `${endpoint}/${application}/${profile}/${label}`;
 }
 
+function createConfigWithHttpResponse(configServerResponse: HttpResponse): Config {
+  if (configServerResponse.error) {
+    throw new Error(JSON.stringify(configServerResponse.error));
+  }
+  return Config.getInstance(JSON.parse(configServerResponse.data));
+}
+
 export function getConfigSync(opts?: ClientRequestOptions): Config {
-  const url = getConfigUrl(opts);
-  const configServerResponse = httpRequestSync(url);
-  return createConfigWithHttpResponse(configServerResponse);
+  try {
+    const url = getConfigUrl(opts);
+    const configServerResponse = httpRequestSync(url);
+    return createConfigWithHttpResponse(configServerResponse);
+  } catch (err) {
+    logger.error('Cannot load config. %o', err instanceof Error ? err.message : err);
+    process.exit(1);
+  }
 }
 
 export async function getConfig(opts?: ClientRequestOptions): Promise<Config> {
-  const url = getConfigUrl(opts);
-  const configServerResponse = await httpRequestAsync(url);
-  return createConfigWithHttpResponse(configServerResponse);
+  try {
+    const url = getConfigUrl(opts);
+    const configServerResponse = await httpRequestAsync(url);
+    return createConfigWithHttpResponse(configServerResponse);
+  } catch (err) {
+    logger.error('Cannot load config. %o', err instanceof Error ? err.message : err);
+    process.exit(1);
+  }
 }
 
 export class Config {
@@ -69,15 +75,14 @@ export class Config {
       return this.instance;
     }
 
-    const isConfigChanged = !ObjectUtil.isEqual(
-      this.instance?.createConfigObject(originalData),
-      this.instance?.configObject
-    );
+    const newConfigObject = this.instance.createConfigObject(originalData);
+    const isConfigChanged = !ObjectUtil.isEqual(newConfigObject, this.instance.configObject);
 
     if (isConfigChanged) {
       this.instance.originalData = originalData;
-      this.instance.configObject = this.instance.createConfigObject(originalData);
+      this.instance.configObject = newConfigObject;
     }
+
     return this.instance;
   }
 
@@ -96,17 +101,63 @@ export class Config {
       : this.configObject[configKey];
   }
 
-  private createConfigObject(originalData: CloudConfigResponse): ObjectType {
-    const propertySources = originalData.propertySources
+  private createConfigObject(originalData: CloudConfigResponse): ConfigObject {
+    const isMultipleApplications = originalData.name.indexOf(',') >= 0;
+
+    const configObject = isMultipleApplications
+      ? this.separateMultipleApplications(originalData)
+      : this.mergeConfigProfiles(originalData.propertySources);
+
+    const configKeys = Object.keys(configObject);
+
+    if (isMultipleApplications) {
+      return configKeys.reduce((mixedConfig: ConfigObject, applicationName) => {
+        const applicationConfig = configObject[applicationName];
+        const configKeys = Object.keys(applicationConfig);
+        mixedConfig[applicationName] = this.mergeFlattenedConfig(configKeys, applicationConfig);
+
+        return mixedConfig;
+      }, {});
+    }
+
+    return this.mergeFlattenedConfig(configKeys, configObject);
+  }
+
+  private separateMultipleApplications(originalData: CloudConfigResponse): ConfigObject {
+    const isDefaultApplicationIncluded = originalData.propertySources.some((propertySource) => {
+      return propertySource.name.includes(DEFAULT_APPLICATION_NAME);
+    });
+
+    const names = originalData.name.split(',');
+    const applicationNames = isDefaultApplicationIncluded ? names.concat(DEFAULT_APPLICATION_NAME) : names;
+
+    const separatedConfig = applicationNames.reduce((config: ConfigObject, applicationName): ConfigObject => {
+      const applicationPropertySourceList = originalData.propertySources.filter((propertySource) => {
+        // extract application name from git address string
+        // ex ) git@github.com:your-workspace/config-repo.git/foo-development.yml >> foo
+        const configApplicationName = propertySource.name.split('/').at(-1)?.split(/(\W)/gi)[0];
+        return configApplicationName === applicationName || configApplicationName === DEFAULT_APPLICATION_NAME;
+      });
+
+      const applicationConfig = this.mergeConfigProfiles(applicationPropertySourceList);
+
+      config[applicationName] = applicationConfig;
+      return config;
+    }, {});
+    return separatedConfig;
+  }
+
+  private mergeConfigProfiles(propertySources: PropertySource[]): ConfigObject {
+    const sources = propertySources
       .map((propertySource: PropertySource) => {
         return propertySource.source;
       })
       .reverse();
+    return ObjectUtil.merge({}, ...sources);
+  }
 
-    const flattenedConfig = ObjectUtil.merge({}, ...propertySources);
-    const flattenedConfigKeys = Object.keys(flattenedConfig);
-
-    const configObjectList: ConfigObject[] = flattenedConfigKeys.reduce((configList: ConfigObject[], currentKey) => {
+  private mergeFlattenedConfig(flattenedKeys: string[], flattenedConfig: ConfigObject): ConfigObject {
+    const configObjectList: ConfigObject[] = flattenedKeys.reduce((configList: ConfigObject[], currentKey) => {
       const value = this.getEnvironmentValueIfExists(currentKey) ?? flattenedConfig[currentKey];
       const isFlattened = currentKey.indexOf('.') >= 0;
 
@@ -117,7 +168,7 @@ export class Config {
       return configList.concat(configObject);
     }, []);
 
-    return ObjectUtil.merge({}, ...configObjectList);
+    return ObjectUtil.merge({}, ...configObjectList) as ConfigObject;
   }
 
   private getEnvironmentValueIfExists(key: string): string | undefined {
